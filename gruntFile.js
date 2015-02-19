900b65067787e82b4e5c0447e0ffee2d17c2c20f
Armaan Ahluwalia <aahluwalia@dataminr.com>
2015-02-19 14:31:54 -0500

    Docs

--- gruntFile.js
+++ gruntFile.js
@@ -3,7 +3,7 @@
 module.exports = function(grunt) {
     var port = '9000';
     var connect = require('./node_modules/grunt-contrib-connect/tasks/connect');
-
+    var docFilePaths = ['mutation.js'];
     grunt.initConfig({
 
         karma: {
@@ -30,12 +30,68 @@ module.exports = function(grunt) {
                 // Gets the port from the connect configuration
                 path: 'http://localhost:<%= connect.all.options.port%>/bin/'
             }
+        },
+        shell: {
+            "install-deps": {
+                command: 'npm install'
+            },
+            "github-pages-delete": {
+                command: 'hasPages=`git branch --list gh-pages`; if [ -n "$hasPages" ]; then git branch -D gh-pages; else echo boo; fi'
+            },
+            "github-pages-checkout": {
+                command: 'git checkout -b gh-pages'
+            },
+            "github-pages-add": {
+                command: 'git add -A'
+            },
+            "github-pages-commit": {
+                command: 'git commit -m "Docs for github"'
+            },
+            "github-pages-push": {
+                command: [
+                    'git push github gh-pages'
+                ]
+            },
+            "movemaindocs": {
+                command: "mv maindocs/* ."
+            },
+            "renamemain": {
+                command: "cp index.md.html index.html"
+            }
+        },
+        clean: {
+            rest: ["*",
+                "!doc",
+                "!node_modules",
+                "!demos",
+                "!maindocs"]
+        },
+        docker: {
+            options: {
+                css: ['docs-style.css']
+                // These options are applied to all tasks
+            },
+            docs: {
+                // Specify `src` and `dest` directly on the task object
+                src: docFilePaths,
+                dest: 'doc'
+            },
+            maindocs: {
+                src: ['index.md'],
+                dest: 'maindocs',
+                options: {
+                    sidebarState: false
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-karma');
     grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-open');
+    grunt.loadNpmTasks('grunt-shell-spawn');
+    grunt.loadNpmTasks('grunt-docker');
 
     // Creates the `server` task
     grunt.registerTask('default',[
@@ -44,5 +100,19 @@ module.exports = function(grunt) {
         'karma:test',
         'open',
         'connect'
+    ]);
+
+    // Creates the `server` task
+    grunt.registerTask('docs', [
+        'shell:install-deps',
+        'shell:github-pages-delete',
+        'shell:github-pages-checkout',
+        'docker:docs',
+        'docker:maindocs',
+        'clean:rest',
+        'shell:movemaindocs',
+        'shell:renamemain',
+        'shell:github-pages-add',
+        'shell:github-pages-commit'
     ]);
 };
